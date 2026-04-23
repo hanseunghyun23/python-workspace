2c2daea1883cb6b9807a89705f1a999c0526e4aa
adminH <hanseunghyun23@gmail.com>
2026-04-23 09:42:26 +0900

    4/22 리스트, 튜플, 집합, 딕셔너리

---
+++ 02_자료형_기초알고리즘/10_사용자정의함수_실습.py
@@ -0,0 +1,36 @@
+
+def problem1():
+    과일들 = ["사과","바나나","포도"]
+    print(len(과일들))
+
+#problem1()
+
+def multiply(a,b):
+    a=int(input())
+    b=int(input())
+    return a*b
+
+#print(f"{a}곱하기{b}는 {multiply(a*b)}입니다")
+
+
+
+
+
+
+def welcome(name):
+    print(f"{name}님, 환영합니다")
+
+ ##welcome(name="김근육")
+
+def average(a, b, c):
+
+    return (a+b+c)/3
+
+res = average(a = int(input()),b = int(input()),c = int(input()))
+print(f"{a}",average.b,average.c, res)
+
+
+def say_hi(name,greeting="좋은하루에요"):
+
+
+
